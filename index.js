c5372ba1e8464ee4f290899131e6c3563ae83118
Jacob Barfield <jake9423@gmail.com>
2020-05-12 20:03:27 -0700

    Creating prompts

--- index.js
+++ index.js
@@ -1,16 +1,62 @@
 const electron = require('electron');
 const dialog = require('electron').remote.dialog;
-// make folderPickerButton activate folderPicker
+const remote = require('electron').remote;
+const BrowserWindow = remote.BrowserWindow;
 
-async function OpenFolderDiolog(){
-     //document.getElementById('folderPicker').click();
+const indexData = require('./data.js');
+
+
+let selectedFilePath;
+
+
+async function OpenFolderDiolog(){    
+    let promise = await (dialog.showOpenDialog({ properties: ['openDirectory']}).then((data) => {selectedFilePath=data.filePaths;}));
     
-    // console.log(document.getElementById('folderPicker').value);
-    // console.log("test");
-    
-    let testing = "failed";
-    let test = await (dialog.showOpenDialog({ properties: ['openDirectory']}).then((data) => {testing=data.filePaths;}));
-    console.log(test);
-    console.log(testing[0]);
-    document.getElementById('directoryDisplay').value = testing[0];
+    indexData.Path = selectedFilePath[0];
+    console.log(indexData.Path);
+    document.getElementById('directoryDisplay').value = indexData.Path;
+}
+
+function subtitlesClick(){
+    if(document.getElementById('chkSubs').checked == true){ 
+
+        const win = new BrowserWindow({
+              height: 200,
+              width: 320,
+              resizable: false,
+              frame: false,
+              webPreferences: {
+                nodeIntegration: true
+              }
+        });
+          
+            win.loadURL('file://' + __dirname +'/subtitlePrompt.html');
+        
+    }
+    else{
+        indexData.SubLang ="eng";
+    }
+    console.log(indexData.SubLang);
+}
+
+function subtitleWindowDone(){
+    indexData.SubLang = "failed";
+    indexData.SubLang = document.getElementById('txtSubLang').value;
+    console.log(indexData.SubLang);
+    window.close();
+}
+
+function subtitleWindowCancel(){
+    indexData.SubLang = "eng";
+    window.close();
+}
+
+function epiClick(){
+    if(document.getElementById('chkEpisode').checked == true){
+         indexData.StartingEp = prompt("Enter starting episode (Default 1)","1");
+    }
+    else{
+        indexData.StartingEp = 1;
+    }
+    console.log(indexData.StartingEp);
 }
